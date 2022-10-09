e8516bf84f60e7f211614aad047b62fa61b506eb
akinnihat <65076591+akinnihat@users.noreply.github.com>
2022-10-09 12:35:02 +0300

    Update simple_epsilon_greedy.py

--- simple_epsilon_greedy.py
+++ simple_epsilon_greedy.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Action:
     def __init__(self, action_id):
-        self.action_no = action_id
+        self.action_id = action_id
         self.estimated_reward_Q = 0                       # Estimated value of action
         self.step_size_N = 0                              # Step size of action
 
@@ -21,7 +21,7 @@ def launch(a1, a2, a3, a4, a5, epsilon_, iter_length):
 
     print("\n---Starting values of actions---")
     for i in range(len(actions)):
-        print("Action " + str(actions[i].action_no) + ": " + str(actions[i].estimated_reward_Q))
+        print("Action " + str(actions[i].action_id) + ": " + str(actions[i].estimated_reward_Q))
 
     print("\n---Experiment---")
     for i in range(iter_length):
@@ -38,7 +38,7 @@ def launch(a1, a2, a3, a4, a5, epsilon_, iter_length):
 
     print("---End values of actions---")
     for i in range(len(actions)):
-        print("Action " + str(actions[i].action_no) + ": " + str("{:.8f}".format(actions[i].estimated_reward_Q)) + 
+        print("Action " + str(actions[i].action_id) + ": " + str("{:.8f}".format(actions[i].estimated_reward_Q)) + 
               " - Step Size: " + str(actions[i].step_size_N))
 
 
